cd01595be40ba84d68098252e11ab8fb973c75ce
RostSuslov <suslovros@gmail.com>
2025-08-22 21:13:30 +0300

    feat: create input desktop

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,1 +1,14 @@
-  
+let button = {
+    content: 'Select 2021-07-26',
+    className: 'custom-button-classname',
+    onClick: (dp) => {
+        let date = new Date('2021-07-26');
+        dp.selectDate(date);
+        dp.setViewDate(date);
+    }
+}
+
+new AirDatepicker('#date', {
+    buttons: [button, 'clear'], // Custom button, and pre-installed 'clear' button
+    position: 'top center'
+})
